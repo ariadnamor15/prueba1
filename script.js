bc55a7fd77a2dbade433dea5ec197e001080a4ae
ariadnamor15 <moran.ariadna15@gmail.com>
2022-05-06 19:44:10 -0300

    probandoAri

---
+++ script.js
@@ -0,0 +1,32 @@
+//1const firstName = prompt("cual es su nombre?");
+//alert(`Hola ${firstName}`);
+
+//2const firstNumber = prompt("por favor elija un numero");
+//const secondNumber = prompt("por favor elija otro numero");
+
+//alert(`esta es la suma de ambos  ${+firstNumber + +secondNumber}`);
+
+//3const firstNumber = prompt("por favor elija un numero");
+//const secondNumber = prompt("por favor elija otro numero");
+
+//alert(`el doble de la suma de ambos es  ${(+firstNumber + +secondNumber) * 2}`);
+
+//4const ancho = prompt("ingrese el ancho");
+//const altura = prompt("ingrese el alto");
+
+//alert(`la superficie es   ${+ancho * +altura}`);
+
+//5const firstName = prompt("cual es su nombre?");
+//alert(`Hola ${firstName.toLowerCase()}`);
+
+//6const secondName = prompt("cual es su apellido?");
+//alert(`Hola tu apellido en mayusculas ${secondName.toUpperCase()}`);
+
+const firstName = prompt("cual es su nombre?");
+const secondName = prompt("cual es su apellido?");
+const Age = prompt("cual es su edad?");
+
+alert(`Estos son sus datos:
+Nombre: ${firstName}
+Apellido: ${secondName} 
+Edad: ${Age}`);
